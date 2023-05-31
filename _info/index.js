b194350813ba86f641d1234062a0e069a59d58f0
Valentine Samoylov <50553611+valentine-samoylov@users.noreply.github.com>
2023-05-31 19:25:59 +0300

    Initial commit

---
+++ _info/index.js
@@ -0,0 +1,9 @@
+// Globals
+
+// SEO
+module.exports = {
+  title: 'Webpack React TS Boilerplate (Tailwind CSS Edition)',
+  description: 'Description',
+  previewImg: 'assets/og-preview.png',
+  url: 'url.com',
+}
